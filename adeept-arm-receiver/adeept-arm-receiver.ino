5b16ed3de54d2428813d32f9f4a84f1acac4639a
avabellaw <avabellawilliams@gmail.com>
2025-12-30 19:16:24 +0000

    2d attempt

--- adeept-arm-receiver/adeept-arm-receiver.ino
+++ adeept-arm-receiver/adeept-arm-receiver.ino
@@ -23,6 +23,8 @@ class ArmServo : public VarSpeedServo {
     }
 }; 
 
+double toDegrees(double radians);
+
 ArmServo servos[5] = { ArmServo(9), ArmServo(6), ArmServo(5), ArmServo(3), ArmServo(11, 180) };
 
 void setup() {
@@ -49,11 +51,30 @@ void loop() {
 
   String operand1 = receivedStr.substring(0, index);
   String operand2 = receivedStr.substring(index+1, length);
-  Serial.println(operand1);  
-  Serial.println(operand2);  
 
-  servos[operand1.toInt()].write(operand2.toInt(), 40, false);
-  servos[2].write(140, 40, false);
+  int x = operand1.toInt();
+  int y = operand2.toInt();
+   
+  double d = sqrt(sq(x) + sq(y));
+  double d1 = 6.4;
+  double d2 = 13.5;
+
+  double angle1acos = acos((sq(d) + sq(d1) - sq(d2)) / (2 * d * d1));
+  double angle1 = atan2(y, x) + angle1acos - angle1acos;
+
+  double angle2 = atan2(y - d1 * sin(angle1), x - d1 * cos(angle1));
+
+  double angle1degrees = toDegrees(angle1);
+  double angle2degrees = toDegrees(angle2);
+
+  servos[1].write(angle1degrees, 40, false);
+  servos[2].write(180 - angle1degrees + angle2degrees, 40, false);
+
+  // servos[operand1.toInt()].write(operand2.toInt(), 40, false);
   
   }
+}
+
+double toDegrees(double radians) {
+  return radians * (180.0 / M_PI);
 }
